535b87ae14ec238ce4ea3a3f0e307a38cd25ed9e
Billy272 <billylaroi@gmail.com>
2023-12-25 08:44:09 +0300

    Update function to handle edge cases
    This commit updates the function to handle edge cases, improving the overall reliability and robustness of the code.

---
+++ Node/exer.js
@@ -0,0 +1,17 @@
+let me = {
+    name: 'Billy',
+    age: 21,
+    height: 190,
+    data : {
+        favoriteColor: 'blue',
+        favoriteFood: 'chocolate balls',
+        hobby: {
+            computer: 'coding',
+            music: 'rap',
+            sport: 'football'
+        }
+    }
+
+}
+
+console.log(me.data);
