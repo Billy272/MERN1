8e228f3bd6dc3a56cf5facbe800cfc3d8fcf3d31
Billy272 <billylaroi@gmail.com>
2023-12-30 13:50:51 +0300

    Add code to iterate over object properties

--- Node/prototype.js
+++ Node/prototype.js
@@ -3,3 +3,16 @@ function Cat(name, weight){
     this.weight = weight;
 }
 
+let Zoe = new Cat("Zoe", 5);
+console.log(Zoe);
+
+let ownProps = [];
+let prototypeProps = [];
+
+for (let property in Zoe){
+    if(Zoe.hasOwnProperty(property)){
+        ownProps.push(property);
+    }else{
+        prototypeProps.push(property);
+    }
+}
