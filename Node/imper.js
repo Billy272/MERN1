aae6ff6fac3747d75b8b8c8c3ae9175631bdf7f3
Billy272 <billylaroi@gmail.com>
2023-12-31 17:58:23 +0300

    Refactor code to improve performance and readability

---
+++ Node/imper.js
@@ -0,0 +1,9 @@
+//using imperatives on codes 
+const Browser = function(tabs,history,bookmarks,extensions) {
+    this.tabs = tabs;
+    this.history = history;
+    this.bookmarks = bookmarks;
+    this.extensions = extensions;
+
+    
+}
