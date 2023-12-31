edf844d2a352c8f4873c821cfca4b8aa7fe80ce6
Billy272 <billylaroi@gmail.com>
2023-12-31 18:03:51 +0300

    Add join method to Browser prototype

--- Node/imper.js
+++ Node/imper.js
@@ -4,6 +4,9 @@ const Browser = function(tabs,history,bookmarks,extensions) {
     this.history = history;
     this.bookmarks = bookmarks;
     this.extensions = extensions;
+}
 
-    
+Browser.prototype.join = function() {
+    this.tabs = this.tabs.concat();
+    return this;
 }
