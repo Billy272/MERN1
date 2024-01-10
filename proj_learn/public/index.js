4c695442f6f0381aaad02cd206528cd88d49abf9
Billy272 <billylaroi@gmail.com>
2024-01-10 12:08:33 +0300

    Refactor index.js to use destructuring and update function calls

--- proj_learn/public/index.js
+++ proj_learn/public/index.js
@@ -1,7 +1,10 @@
 require('./iife')
+
 const math = require('./add')
-console.log(math.add(4, 2));
-console.log(math.multiply(4, 2));
+const{add, multiply} = math
+console.log(add(4, 2));
+console.log(multiply(4, 2));
+
 const guy = require('./app');
 const you = new guy("Willy", 21);
 console.log(you.getName());
