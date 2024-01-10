c432761d1b0f8a55d96a79531cb491651acfd289
Billy272 <billylaroi@gmail.com>
2024-01-10 09:39:16 +0300

    Update app.js to log a message

--- proj_learn/public/app.js
+++ proj_learn/public/app.js
@@ -1,11 +1,1 @@
-import express from 'express';
-const app = express();
-const port = 3000;
-
-app.get('/', (_req, res)=>{
-    res.send('Hello World');
-});
-
-app.listen(port, () => {
-    console.log(`Listening at sever port ${port} `);
-});
+console.log("Hello from app.js");
