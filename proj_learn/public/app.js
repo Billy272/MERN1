05a4e9638e536cd8f1b5feadb076ca7b957f460f
Billy272 <billylaroi@gmail.com>
2024-01-10 11:35:59 +0300

    Add person class and module caching

--- proj_learn/public/app.js
+++ proj_learn/public/app.js
@@ -3,3 +3,33 @@ const add = require('./add')
 console.log("Hello from app.js");
 const sum = add(4, 2);
 console.log(sum);
+
+//Module caching
+class person {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    getAge(){
+        return this.age;
+    }
+
+    setName(name){
+        this.name = name;
+    }
+
+    setAge(age){
+        this.age = age;
+    }
+}
+
+const me = new person("Billy", 21);
+console.log(me.getName());
+console.log(me.getAge());
+
+module.exports = person;
--- proj_learn/public/index.js
+++ proj_learn/public/index.js
@@ -1,2 +1,6 @@
 require('./iife')
 require('./add')
+const guy = require('./app');
+const you = new guy("Willy", 21);
+console.log(you.getName());
+console.log(you.getAge());
