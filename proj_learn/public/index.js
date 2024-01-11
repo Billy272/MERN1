ab87d210694e4d5130894dd5f06b4b039dcc976a
Billy272 <billylaroi@gmail.com>
2024-01-11 11:13:04 +0300

    Add module imports and path operations to index.js

--- proj_learn/public/index.js
+++ proj_learn/public/index.js
@@ -14,3 +14,21 @@ console.log(you.getAge());
 const data = require("./data.json");
 console.log(data);
 console.log("The main version is",data.version);
+
+//importing a built-in module
+const path = require("node:path");
+console.log(__filename);
+console.log(__dirname);
+
+console.log(path.basename(__filename));
+console.log(path.basename(__dirname));
+
+console.log(path.extname(__filename));
+console.log(path.extname(__dirname));
+
+console.log(path.parse(__filename));
+
+console.log(path.format(path.parse(__filename)));
+
+//Absolute path
+console.log(path.isAbsolute(__filename));
