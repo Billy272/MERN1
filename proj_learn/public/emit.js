e96076494896c8ede42e4f08d2ff1f202deee878
Billy272 <billylaroi@gmail.com>
2024-01-12 22:06:59 +0300

    commit

--- proj_learn/public/emit.js
+++ proj_learn/public/emit.js
@@ -7,4 +7,14 @@ emmiter.on("Order food", (quantity, size)=> {
     console.log(`Order accepted of ${quantity} pieces,the size is ${size}. Food on delivery`);
 });
 
+emmiter.on("Order food", (size) => {
+    if(size == "large"){
+        console.log("You get a free drink and an extra burger.");
+    } else if (size == "medium"){
+        console.log("You get a free drink.");
+    } else {
+        console.log("Thanks from buying from us.");
+    }
+})
+
 emmiter.emit("Order food", 4, "large");
