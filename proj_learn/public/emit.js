862110415a2bb3e1d103267cbda7203fb42cab07
Billy272 <billylaroi@gmail.com>
2024-01-12 22:53:11 +0300

    Refactor food ordering code

--- proj_learn/public/emit.js
+++ proj_learn/public/emit.js
@@ -1,20 +1,26 @@
+const food_shop = require("./food_shop");
+
+const ordered = new food_shop();
+
+ordered.order();
+ordered.displayOrder();
 //use of emitter
-const EventEmmiter = require("node:events");
+// const EventEmmiter = require("node:events");
 
-const emmiter  = new EventEmmiter();
+// const emmiter  = new EventEmmiter();
 
-emmiter.on("Order food", (quantity, size)=> {
-    console.log(`Order accepted of ${quantity} pieces,the size is ${size}. Food on delivery`);
-});
+// emmiter.on("Order food", (quantity, size)=> {
+//     console.log(`Order accepted of ${quantity} pieces,the size is ${size}. Food on delivery`);
+// });
 
-emmiter.on("Order food", (size) => {
-    if(size === "large"){
-        console.log("You get a free drink and an extra burger.");
-    } else if (size === "medium"){
-        console.log("You get a free drink.");
-    } else {
-        console.log("Thank you from buying from us.");
-    }
-})
+// emmiter.on("Order food", (size) => {
+//     if(size === "large"){
+//         console.log("You get a free drink and an extra burger.");
+//     } else if (size === "medium"){
+//         console.log("You get a free drink.");
+//     } else {
+//         console.log("Thank you from buying from us.");
+//     }
+// })
 
-emmiter.emit("Order food", 4, "large");
+// emmiter.emit("Order food", 4, "large");
