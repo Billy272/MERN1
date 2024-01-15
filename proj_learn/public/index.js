e471327064700facb661904387b08eeb0aa67c9a
Billy272 <billylaroi@gmail.com>
2024-01-15 17:17:31 +0300

    Fix file path in index.js

--- proj_learn/public/index.js
+++ proj_learn/public/index.js
@@ -3,7 +3,7 @@ const fs = require('node:fs');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/html"}); //Always specify the content type
-    fs.createReadStream("./index.html").pipe(res);
+    fs.createReadStream(__dirname + "./index.html").pipe(res);
     // const html = fs.readFileSync("./index.html", "utf-8");
     // res.end(html);
 });
