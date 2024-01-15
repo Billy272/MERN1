ea7dc2687584b2616e5b94f97f94874547930413
Billy272 <billylaroi@gmail.com>
2024-01-15 13:15:58 +0300

    Remove unnecessary files and code

---
+++ Node_practice/index.js
@@ -0,0 +1,59 @@
+require('./iife')
+
+const math = require('./add')
+const{add, multiply} = math
+console.log(add(4, 2));
+console.log(multiply(4, 2));
+
+const guy = require('./app');
+const you = new guy("Willy", 21);
+console.log(you.getName());
+console.log(you.getAge());
+
+//importing json files
+const data = require("./data.json");
+console.log(data);
+console.log("The main version is",data.version);
+
+//importing a built-in module
+const path = require("node:path");
+console.log(__filename);
+console.log(__dirname);
+
+console.log(path.basename(__filename));
+console.log(path.basename(__dirname));
+
+console.log(path.extname(__filename));
+console.log(path.extname(__dirname));
+
+console.log(path.parse(__filename));
+
+console.log(path.format(path.parse(__filename)));
+
+//Absolute path
+console.log(path.isAbsolute(__filename));
+
+//joins paths
+console.log(path.join(__dirname, "test", "hello.html"));
+console.log(path.resolve(__dirname, "../test", "hello.html"));
+
+//use of function greet
+function greet(name){
+    console.log(`Hi ${name}`);
+}
+
+greet("Billy");
+//callback function on greet
+function salute(greetName){
+    const name = "Delhi";
+    greetName(name);
+}
+
+salute(greet);
+
+//creating a buffer that holds a buffer
+const buf = new Buffer.from("Hello");
+
+buf.write("World");
+console.log(buf);
+console.log(buf.toJSON());
