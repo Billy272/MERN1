af5cacd2b4b5c5cfcf7be717de1f4f6012bec4f7
Billy272 <billylaroi@gmail.com>
2024-01-15 17:10:36 +0300

    Refactor file serving logic in index.js

--- proj_learn/public/index.js
+++ proj_learn/public/index.js
@@ -3,8 +3,9 @@ const fs = require('node:fs');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/html"}); //Always specify the content type
-    const html = fs.readFileSync("./index.html", "utf-8");
-    res.end(html);
+    fs.createReadStream("./index.html").pipe(res);
+    // const html = fs.readFileSync("./index.html", "utf-8");
+    // res.end(html);
 });
 
 server.listen(3000, () => {
