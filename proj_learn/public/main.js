fad8336113e451cb8e28e2ebdcced38338447543
Billy272 <billylaroi@gmail.com>
2024-01-15 11:17:50 +0300

    Add asynchronous file reading and logging statements
    This commit adds asynchronous file reading and logging statements to the main.js file in the proj_learn/public directory. The new code demonstrates how JavaScript handles asynchronous operations and proves that the application can handle multiple user requests without lagging.

--- proj_learn/public/main.js
+++ proj_learn/public/main.js
@@ -1,6 +1,22 @@
 //dealing with fs module
 const fs = require("node:fs");
 
+console.log("First statement");
 const fileContent = fs.readFileSync("./file.txt", "utf-8");
 
 console.log(fileContent);
+
+console.log("2nd statement");
+fs.readFile("./file.txt","utf-8", (error, data) => {
+    if(error){
+        console.log(error);
+    } else {
+        console.log(data);
+    }
+})
+
+console.log("This proves that JS is asynchronous");
+
+//This whole process above proves that an applicaton cannot lag 
+//when many users are using it at the same time.
+//The process asynchronusly handles the requests of the users.
