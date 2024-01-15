7ac3244ea04bde691adbf2abc2a6a74851529787
Billy272 <billylaroi@gmail.com>
2024-01-15 16:40:51 +0300

    Add JSON response and update content type

--- proj_learn/public/index.js
+++ proj_learn/public/index.js
@@ -1,10 +1,19 @@
 const http = require('node:http');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "text/plain"}); //Always specify the content type
-    res.end("Welcome to node.js")
+
+    const me = {
+        name: "Billy",
+        age: 21,
+        location: "Nairobi"
+    };
+
+    res.writeHead(200, {"Content-Type": "application/json"}); //Always specify the content type
+    res.end(JSON.stringify(me));
 });
 
 server.listen(3000, () => {
     console.log("Server is working on port 3000");
 });
+
+//This is my first API with JS. I am so excited.
