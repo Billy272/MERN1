fe131bf7da0e20dd366ca97da89d53face6dd81e
Billy272 <billylaroi@gmail.com>
2024-01-16 10:07:15 +0300

    Update error response content type

--- proj_learn/public/index.js
+++ proj_learn/public/index.js
@@ -1,4 +1,3 @@
-
 const http = require('node:http');
 const fs = require('node:fs');
 
@@ -32,7 +31,7 @@ const server = http.createServer((req, res) => {
         };
         res.end(JSON.stringify(data));
     } else {
-        res.writeHead(404);
+        res.writeHead(404, {"Content-Type": "text/plain" });
         res.end("Page not found.");
     }
 });
