fe6d9553eb4e1a4913a29371c07074cc9e3f5ed0
Billy272 <billylaroi@gmail.com>
2024-01-17 20:34:13 +0300

    Commented out unused code and added setImmediate after reading file

--- proj_learn/public/ind.js
+++ proj_learn/public/ind.js
@@ -1,39 +1,39 @@
-setTimeout(() =>
-    console.log("This is set timeout 1")
-, 1000);
-setTimeout(() => {
-    console.log("This is set timeout 2");
-    setTimeout(() => 
-        console.log("This is set timeout inside another timeout")
-    );
-}, 500);
-setTimeout(() => 
-    console.log("This is set timeout 3")
-, 1);
+// setTimeout(() =>
+//     console.log("This is set timeout 1")
+// , 1000);
+// setTimeout(() => {
+//     console.log("This is set timeout 2");
+//     setTimeout(() => 
+//         console.log("This is set timeout inside another timeout")
+//     );
+// }, 500);
+// setTimeout(() => 
+//     console.log("This is set timeout 3")
+// , 1);
 
-process.nextTick(() => {
-    console.log("This is Next tick 1");
-})
-process.nextTick(() => {
-    console.log("This is Next tick 2");
-    process.nextTick(() => {
-        console.log("This is Next tick inside another tick function");
-    })
-})
-process.nextTick(() => {
-    console.log("This is Next tick 3");
-})
+// process.nextTick(() => {
+//     console.log("This is Next tick 1");
+// })
+// process.nextTick(() => {
+//     console.log("This is Next tick 2");
+//     process.nextTick(() => {
+//         console.log("This is Next tick inside another tick function");
+//     })
+// })
+// process.nextTick(() => {
+//     console.log("This is Next tick 3");
+// })
 
-Promise.resolve().then(() => {
-    console.log("Promise resolved 1");
-});
+// Promise.resolve().then(() => {
+//     console.log("Promise resolved 1");
+// });
 
-Promise.resolve().then(() => {
-    console.log("Promise resolved 2");
-    Promise.resolve().then(() => {
-        console.log("Promise resolved inside another promise");
-    })
-})
+// Promise.resolve().then(() => {
+//     console.log("Promise resolved 2");
+//     Promise.resolve().then(() => {
+//         console.log("Promise resolved inside another promise");
+//     })
+// })
 
 Promise.resolve().then(() => {
     console.log("Promise resolved 3");
@@ -57,5 +57,9 @@ Promise.resolve().then(() => {
 setTimeout(() =>
     console.log("Set timeout after reading file")
 );
+
+setImmediate(() => {
+    console.log("Set immediate after reading file");
+})
 
 for (let i = 0; i < 2000000; i++){}
