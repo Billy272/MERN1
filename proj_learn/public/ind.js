0c57d2878af6daee40476f5306c00891f99b4029
Billy272 <billylaroi@gmail.com>
2024-01-17 20:48:42 +0300

    commit

--- proj_learn/public/ind.js
+++ proj_learn/public/ind.js
@@ -1,43 +1,43 @@
-// setTimeout(() =>
-//     console.log("This is set timeout 1")
-// , 1000);
-// setTimeout(() => {
-//     console.log("This is set timeout 2");
-//     setTimeout(() => 
-//         console.log("This is set timeout inside another timeout")
-//     );
-// }, 500);
-// setTimeout(() => 
-//     console.log("This is set timeout 3")
-// , 1);
+setTimeout(() =>
+    console.log("This is set timeout 1")
+, 1000);
+setTimeout(() => {
+    console.log("This is set timeout 2");
+    setTimeout(() => 
+        console.log("This is set timeout inside another timeout")
+    );
+}, 500);
+setTimeout(() => 
+    console.log("This is set timeout 3")
+, 1);
 
-// process.nextTick(() => {
-//     console.log("This is Next tick 1");
-// })
-// process.nextTick(() => {
-//     console.log("This is Next tick 2");
-//     process.nextTick(() => {
-//         console.log("This is Next tick inside another tick function");
-//     })
-// })
-// process.nextTick(() => {
-//     console.log("This is Next tick 3");
-// })
+process.nextTick(() => {
+    console.log("This is Next tick 1");
+})
+process.nextTick(() => {
+    console.log("This is Next tick 2");
+    process.nextTick(() => {
+        console.log("This is Next tick inside another tick function");
+    })
+})
+process.nextTick(() => {
+    console.log("This is Next tick 3");
+})
 
-// Promise.resolve().then(() => {
-//     console.log("Promise resolved 1");
-// });
+Promise.resolve().then(() => {
+    console.log("Promise resolved 1");
+});
 
-// Promise.resolve().then(() => {
-//     console.log("Promise resolved 2");
-//     Promise.resolve().then(() => {
-//         console.log("Promise resolved inside another promise");
-//     })
-// })
+Promise.resolve().then(() => {
+    console.log("Promise resolved 2");
+    Promise.resolve().then(() => {
+        console.log("Promise resolved inside another promise");
+    })
+})
 
-// Promise.resolve().then(() => {
-//     console.log("Promise resolved 3");
-// })
+Promise.resolve().then(() => {
+    console.log("Promise resolved 3");
+})
 
 //I/O queue
 const fs = require("node:fs");
