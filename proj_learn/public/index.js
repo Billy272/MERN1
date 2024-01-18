d5b84b25873b093b754f262300bc062aca134615
Billy272 <billylaroi@gmail.com>
2024-01-18 18:01:09 +0300

    Refactor server code to render dynamic HTML

--- proj_learn/public/index.js
+++ proj_learn/public/index.js
@@ -1,38 +1,38 @@
 const http = require('node:http');
 const fs = require('node:fs');
 
-// const server = http.createServer((req, res) => {
-//     const name = "Billy";
-//     res.writeHead(200, {"Content-Type": "text/html" }); //Always specify the content type
-//     // fs.createReadStream(__dirname + "./index.html").pipe(res);
-//     let html = fs.readFileSync("./index.html", "utf-8");
-//     html = html.replace("{{name}}", name);
-//     res.end(html);
-// });
-
 const server = http.createServer((req, res) => {
-    if (req.url === "/") {
-        res.writeHead(200, {"Content-Type": "text/plain" });
-        res.end("Welcome to the home page");
-    } else if (req.url === "/about") {
-        res.writeHead(200, {"Content-Type": "text/plain" });
-        res.end("This is the about page");
-    } else if (req.url === "/contact"){
-        res.writeHead(200, {"Content-Type" : "text/plain" });
-        res.end("This is the contact page");
-    } else if (req.url === "/api") {
-        res.writeHead(200, {"Content-Type": "application/json" });
-        const data = {
-            name: "Billy",
-            age: 21,
-            job: "Programmer"
-        };
-        res.end(JSON.stringify(data));
-    } else {
-        res.writeHead(404, {"Content-Type": "text/plain" });
-        res.end("Error 404.Page not found.");
-    }
+    const name = "Billy";
+    res.writeHead(200, {"Content-Type": "text/html" }); //Always specify the content type
+    // fs.createReadStream(__dirname + "./index.html").pipe(res);
+    let html = fs.readFileSync("./index.html", "utf-8");
+    html = html.replace("{{name}}", name);
+    res.end(html);
 });
+
+// const server = http.createServer((req, res) => {
+//     if (req.url === "/") {
+//         res.writeHead(200, {"Content-Type": "text/plain" });
+//         res.end("Welcome to the home page");
+//     } else if (req.url === "/about") {
+//         res.writeHead(200, {"Content-Type": "text/plain" });
+//         res.end("This is the about page");
+//     } else if (req.url === "/contact"){
+//         res.writeHead(200, {"Content-Type" : "text/plain" });
+//         res.end("This is the contact page");
+//     } else if (req.url === "/api") {
+//         res.writeHead(200, {"Content-Type": "application/json" });
+//         const data = {
+//             name: "Billy",
+//             age: 21,
+//             job: "Programmer"
+//         };
+//         res.end(JSON.stringify(data));
+//     } else {
+//         res.writeHead(404, {"Content-Type": "text/plain" });
+//         res.end("Error 404.Page not found.");
+//     }
+// });
 
 server.listen(3000, () => {
     console.log("Server is working on port 3000");
