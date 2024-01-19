8c7ce64b303b4b557bbc4b096ccfa6af305d0266
Billy272 <billylaroi@gmail.com>
2024-01-19 23:46:49 +0300

    Add worker thread for slow page

--- Node_practice/thread.js
+++ Node_practice/thread.js
@@ -1,16 +1,16 @@
 const http = require('http');
+const { Worker } = require('node:worker_threads')
 
 const server = http.createServer((req, res) => {
     if(req.url === '/'){
         res.writeHead(200, {"Content-Type":"text/plain"});
         res.end("Welcome to the home page");
     } else if (req.url === "/slow-page"){
-        let j = 0;
-        for (let i=0; i< 50000000; i++){
-            j++;
-        }
-        res.writeHead(200, {"Content-Type": "text/plain"});
-        res.end(`Welcome to the slow page ${j}`);
+        const worker = new Worker("./worker-thread.js");
+        worker.on("message", (j) => {
+            res.writeHead(200, {"Content-Type": "text/plain"});
+            res.end(`Welcome to the slow page ${j}`);
+        })
     }
 })
 
